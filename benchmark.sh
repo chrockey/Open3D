90d02453f76316ccdebae8ea7fe5099d8a6f860f
Yixing Lao <yixing.lao@gmail.com>
2021-07-05 01:33:34 -0700

    run one

--- benchmark.sh
+++ benchmark.sh
@@ -3,17 +3,6 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BUILD_DIR=${SCRIPT_DIR}/build
-
-# # Number of CPU cores, not counting hyper-threading:
-# # https://stackoverflow.com/a/6481016/1255535
-# #
-# # Typically, set max # of threads to the # of physical cores, not logical:
-# # https://www.thunderheadeng.com/2014/08/openmp-benchmarks/
-# # https://stackoverflow.com/a/36959375/1255535
-# NUM_CORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
-# export OMP_NUM_THREADS=${NUM_CORES}
-# echo "OMP_NUM_THREADS: ${OMP_NUM_THREADS}"
-
 NPROC=$(nproc)
 
 pushd ${BUILD_DIR}
@@ -28,7 +17,8 @@ rm -rf ${OUT_FILE}
 touch ${OUT_FILE}
 
 # Run all tensor-related benchmarks
-BENCHMARK_FILTER="FromLegacyPointCloud|ToLegacyPointCloud|VoxelDownSample/core|Odometry|RegistrationICP"
+# If there are multiple sets of benchmarks of the same code, only run one of them
+BENCHMARK_FILTER="FromLegacyPointCloud|ToLegacyPointCloud|VoxelDownSample/core::HashmapBackend::TBB_0_01|Odometry|BenchmarkRegistrationICP/"
 
 # Benchmark
 echo "Running benchmarks from 1 to ${NPROC} threads."
@@ -41,10 +31,8 @@ do
 
     if [[ ${OMP_NUM_THREADS} = ${NPROC} ]]; then
         # Don't specify OMP_NUM_THREADS if it is the maximum already
-        echo "special"
         ./bin/benchmarks --benchmark_filter=${BENCHMARK_FILTER} >> ${OUT_FILE} 2>&1
     else
-        echo "regular"
         OMP_NUM_THREADS=${OMP_NUM_THREADS} ./bin/benchmarks --benchmark_filter=${BENCHMARK_FILTER} >> ${OUT_FILE} 2>&1
     fi
 
